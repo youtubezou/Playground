388c38f0c9ec1369a84f4dc3f6100510315314e0
CyberZHG <CyberZHG@gmail.com>
2015-11-29 22:03:00 +0800

    Lab 3 Part A

--- 6.824/src/shardmaster/server.go
+++ 6.824/src/shardmaster/server.go
@@ -9,9 +9,12 @@ import "paxos"
 import "sync"
 import "sync/atomic"
 import "os"
+import "time"
 import "syscall"
 import "encoding/gob"
 import "math/rand"
+import crand "crypto/rand"
+import "math/big"
 
 type ShardMaster struct {
 	mu         sync.Mutex
@@ -21,36 +24,220 @@ type ShardMaster struct {
 	unreliable int32 // for testing
 	px         *paxos.Paxos
 
-	configs []Config // indexed by config num
+	configs   []Config // indexed by config num
+	configNum int
+	confirmed int
 }
 
+const (
+	OpJoin  = "Join"
+	OpLeave = "Leave"
+	OpMove  = "Move"
+	OpQuery = "Query"
+)
+
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := crand.Int(crand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
 
 type Op struct {
-	// Your data here.
+	Type    string
+	Token   int64
+	GID     int64
+	Servers []string
+	Shard   int
+	Num     int
 }
 
+func (sm *ShardMaster) AriseOp(op Op) {
+	op.Token = nrand()
+	for {
+		sm.px.Start(sm.confirmed+1, op)
+		to := 10 * time.Millisecond
+		for {
+			fate, value := sm.px.Status(sm.confirmed + 1)
+			if fate == paxos.Decided {
+				sm.ConfirmOp(value.(Op))
+				if value.(Op).Token == op.Token {
+					return
+				} else {
+					break
+				}
+			}
+			time.Sleep(to)
+			if to < 10*time.Second {
+				to *= 2
+			}
+		}
+	}
+}
+
+func (sm *ShardMaster) LastestConfig() *Config {
+	return &sm.configs[sm.configNum]
+}
+
+func (sm *ShardMaster) NewConfig() *Config {
+	config := sm.LastestConfig()
+	var newConfig Config
+	newConfig.Num = config.Num + 1
+	newConfig.Shards = [NShards]int64{}
+	for shard, gid := range config.Shards {
+		newConfig.Shards[shard] = gid
+	}
+	newConfig.Groups = make(map[int64][]string)
+	for gid, servers := range config.Groups {
+		newConfig.Groups[gid] = []string{}
+		for _, server := range servers {
+			newConfig.Groups[gid] = append(newConfig.Groups[gid], server)
+		}
+	}
+	sm.configNum++
+	sm.configs = append(sm.configs, newConfig)
+	return &sm.configs[sm.configNum]
+}
+
+func (sm *ShardMaster) GetGroupShards() map[int64][]int {
+	shards := make(map[int64][]int)
+	for gid, _ := range sm.LastestConfig().Groups {
+		shards[gid] = []int{}
+	}
+	for shard, gid := range sm.LastestConfig().Shards {
+		shards[gid] = append(shards[gid], shard)
+	}
+	return shards
+}
+
+func (sm *ShardMaster) Rebalance(groupShards map[int64][]int, freeShards []int) {
+	config := sm.LastestConfig()
+	if len(config.Groups) == 0 {
+		for idx, _ := range config.Shards {
+			config.Shards[idx] = 0
+		}
+		return
+	}
+	invalidShards, exists := groupShards[0]
+	if exists {
+		for _, shard := range invalidShards {
+			freeShards = append(freeShards, shard)
+		}
+		delete(groupShards, 0)
+	}
+	aveNum := NShards / len(config.Groups)
+	aboveNum := NShards % len(config.Groups)
+	//fmt.Println("[Rebalancing]", config, groupShards, freeShards, aveNum, aboveNum)
+	for _, shards := range groupShards {
+		bottomLine := aveNum
+		if aboveNum > 0 {
+			bottomLine++
+		}
+		if len(shards) >= bottomLine {
+			for i := bottomLine; i < len(shards); i++ {
+				freeShards = append(freeShards, shards[i])
+			}
+			aboveNum--
+		}
+	}
+	idx := 0
+	for gid, shards := range groupShards {
+		topLine := aveNum
+		if aboveNum > 0 {
+			topLine++
+		}
+		if len(shards) < topLine {
+			for i := len(shards); i < topLine; i++ {
+				config.Shards[freeShards[idx]] = gid
+				idx++
+			}
+			aboveNum--
+		}
+	}
+	//fmt.Println("[Balanced]", config.Shards)
+}
+
+func (sm *ShardMaster) ConfirmOp(op Op) {
+	switch op.Type {
+	case OpJoin:
+		_, exists := sm.LastestConfig().Groups[op.GID]
+		if !exists {
+			config := sm.NewConfig()
+			config.Groups[op.GID] = op.Servers
+			groupShards := sm.GetGroupShards()
+			groupShards[op.GID] = []int{}
+			freeShards := []int{}
+			sm.Rebalance(groupShards, freeShards)
+		}
+	case OpLeave:
+		_, exists := sm.LastestConfig().Groups[op.GID]
+		if exists {
+			config := sm.NewConfig()
+			delete(config.Groups, op.GID)
+			groupShards := sm.GetGroupShards()
+			freeShards := groupShards[op.GID]
+			delete(groupShards, op.GID)
+			sm.Rebalance(groupShards, freeShards)
+		}
+	case OpMove:
+		config := sm.NewConfig()
+		config.Shards[op.Shard] = op.GID
+	case OpQuery:
+	}
+	sm.confirmed++
+	sm.px.Done(sm.confirmed)
+	//fmt.Println("[Confirmed]", sm.me, sm.confirmed, op)
+}
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) error {
-	// Your code here.
-
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	//fmt.Println("[Join]", sm.me, args)
+	var op Op
+	op.Type = OpJoin
+	op.GID = args.GID
+	op.Servers = args.Servers
+	sm.AriseOp(op)
 	return nil
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) error {
-	// Your code here.
-
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	//fmt.Println("[Leave]", sm.me, args)
+	var op Op
+	op.Type = OpLeave
+	op.GID = args.GID
+	sm.AriseOp(op)
 	return nil
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) error {
-	// Your code here.
-
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	//fmt.Println("[Move]", sm.me, args)
+	var op Op
+	op.Type = OpMove
+	op.Shard = args.Shard
+	op.GID = args.GID
+	sm.AriseOp(op)
 	return nil
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) error {
-	// Your code here.
-
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	//fmt.Println("[Query]", sm.me, args)
+	var op Op
+	op.Type = OpQuery
+	op.Num = args.Num
+	sm.AriseOp(op)
+	if args.Num == -1 || args.Num > sm.configNum {
+		reply.Config = *sm.LastestConfig()
+	} else {
+		reply.Config = sm.configs[args.Num]
+	}
+	//fmt.Println("[Reply]", sm.me, reply)
 	return nil
 }
 
@@ -91,6 +278,9 @@ func StartServer(servers []string, me int) *ShardMaster {
 
 	sm.configs = make([]Config, 1)
 	sm.configs[0].Groups = map[int64][]string{}
+
+	sm.configNum = 0
+	sm.confirmed = -1
 
 	rpcs := rpc.NewServer()
 
