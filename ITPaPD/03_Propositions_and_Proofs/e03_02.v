b9d219ddb0de323448cb742760669f7fdebb5fc0
CyberZHG <CyberZHG@gmail.com>
2017-05-09 10:35:01 +0800

    Structure of an Interactive Proof.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- ITPaPD/03_Propositions_and_Proofs/e03_02.v
+++ ITPaPD/03_Propositions_and_Proofs/e03_02.v
@@ -25,4 +25,61 @@ Proof.
 Qed.
 Print imp_trans.
 
+Lemma imp_perm : (P -> Q -> R) -> (Q -> P -> R).
+Proof.
+    intros X q p.
+    apply X.
+    assumption.
+    assumption.
+Qed.
+Print imp_perm.
+
+Lemma ignore_Q : (P -> R) -> P -> Q -> R.
+Proof.
+    intros X p q.
+    apply X.
+    assumption.
+Qed.
+Print ignore_Q.
+
+Lemma delta_imp : (P -> P -> Q) -> P -> Q.
+Proof.
+    intros X p.
+    apply X.
+    assumption.
+    assumption.
+Qed.
+Print delta_imp.
+
+Lemma delta_impR : (P -> Q) -> (P -> P -> Q).
+Proof.
+    intros X p.
+    assumption.
+Qed.
+Print delta_impR.
+
+Lemma diamond : (P -> Q) -> (P -> R) -> (Q -> R -> T) -> P -> T.
+Proof.
+    intros X Y Z p.
+    apply Z.
+    apply X.
+    assumption.
+    apply Y.
+    assumption.
+Qed.
+Print diamond.
+
+Lemma weak_peirce : ((((P -> Q) -> P) -> P) -> Q) -> Q.
+Proof.
+    intros X.
+    apply X.
+    intros Y.
+    apply Y.
+    intros p.
+    apply X.
+    intros Z.
+    assumption.
+Qed.
+Print weak_peirce.
+
 End Section_03_02.
