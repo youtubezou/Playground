46b0970ccdfe39a270af2c5220db0bba6b5152f0
CyberZHG <CyberZHG@gmail.com>
2017-05-10 10:39:10 +0800

    Composing tactics.

--- ITPaPD/03_Propositions_and_Proofs/e03_03.v
+++ ITPaPD/03_Propositions_and_Proofs/e03_03.v
@@ -1,85 +1,59 @@
-Section Section_03_02.
+Section Section_03_03.
 
 Variables P Q R T : Prop.
 
 Lemma id_P : P -> P.
 Proof.
-    intro p.
-    assumption.
+    intro p; assumption.
 Qed.
 Print id_P.
 
 Lemma id_PP : (P -> P) -> (P -> P).
 Proof.
-    intro S.
-    assumption.
+    intro S; assumption.
 Qed.
 Print id_PP.
 
 Lemma imp_trans : (P -> Q) -> (Q -> R) -> P -> R.
 Proof.
-    intros X Y p.
-    apply Y.
-    apply X.
-    assumption.
+    intros X Y p; apply Y; apply X; assumption.
 Qed.
 Print imp_trans.
 
 Lemma imp_perm : (P -> Q -> R) -> (Q -> P -> R).
 Proof.
-    intros X q p.
-    apply X.
-    assumption.
-    assumption.
+    intros X q p; apply X; assumption; assumption.
 Qed.
 Print imp_perm.
 
 Lemma ignore_Q : (P -> R) -> P -> Q -> R.
 Proof.
-    intros X p q.
-    apply X.
-    assumption.
+    intros X p q; apply X; assumption.
 Qed.
 Print ignore_Q.
 
 Lemma delta_imp : (P -> P -> Q) -> P -> Q.
 Proof.
-    intros X p.
-    apply X.
-    assumption.
-    assumption.
+    intros X p; apply X; assumption; assumption.
 Qed.
 Print delta_imp.
 
 Lemma delta_impR : (P -> Q) -> (P -> P -> Q).
 Proof.
-    intros X p.
-    assumption.
+    intros X p; assumption.
 Qed.
 Print delta_impR.
 
 Lemma diamond : (P -> Q) -> (P -> R) -> (Q -> R -> T) -> P -> T.
 Proof.
-    intros X Y Z p.
-    apply Z.
-    apply X.
-    assumption.
-    apply Y.
-    assumption.
+    intros X Y Z p; apply Z; [apply X | apply Y]; assumption.
 Qed.
 Print diamond.
 
 Lemma weak_peirce : ((((P -> Q) -> P) -> P) -> Q) -> Q.
 Proof.
-    intros X.
-    apply X.
-    intros Y.
-    apply Y.
-    intros p.
-    apply X.
-    intros Z.
-    assumption.
+    intros X; apply X; intros Y; apply Y; intros p; apply X; intros Z; assumption.
 Qed.
 Print weak_peirce.
 
-End Section_03_02.
+End Section_03_03.
